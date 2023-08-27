aec1b0b238dece05415fe125ac06fb170c6c7db0
lodo4ka1337 <volegovnik2010@mail.ru>
2023-08-27 13:11:44 +0700

    init commit (dependency definition)

---
+++ src/main/java/com/lodo4ka/shopElectronics/ShopElectronicsApplication.java
@@ -0,0 +1,13 @@
+package com.lodo4ka.shopElectronics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopElectronicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopElectronicsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lodo4ka/shopElectronics/ShopElectronicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lodo4ka.shopElectronics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopElectronicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
