1ec2faf035e499d21403a0f1dcb0f98f13ede4cc
lodo4ka1337 <volegovnik2010@mail.ru>
2023-08-27 19:35:42 +0700

    Modified application.properties Modified db.changelog-master.yaml Modified pom.xml Added model Showcase.java class

---
+++ src/main/java/com/lodo4ka/shopElectronics/persistance/model/Showcase.java
@@ -0,0 +1,84 @@
+package com.lodo4ka.shopElectronics.persistance.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+import java.util.Date;
+import java.util.UUID;
+
+
+@Entity
+@Table(name = "showcases")
+public class Showcase implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true, nullable = false)
+    private UUID id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "address", unique = true, nullable = false)
+    private String address;
+
+    @Column(name = "type", nullable = false)
+    private String type;
+
+    @Column(name = "creation_date", nullable = false)
+    private Date creation_date;
+
+    @Column(name = "last_update_date", nullable = false)
+    private  Date last_update_date;
+
+    public Showcase() {
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Date getCreation_date() {
+        return creation_date;
+    }
+
+    public void setCreation_date(Date creation_date) {
+        this.creation_date = creation_date;
+    }
+
+    public Date getLast_update_date() {
+        return last_update_date;
+    }
+
+    public void setLast_update_date(Date last_update_date) {
+        this.last_update_date = last_update_date;
+    }
+}
